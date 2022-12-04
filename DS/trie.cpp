e8c219f41c89e025e124f5987e11def992cc37d0
ajaysinghpanwar2002 <ajaysingh07032003@gmail.com>
2022-12-04 11:42:03 +0530

    Trie

---
+++ DS/trie.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+/*
+
+reTRIEval: to retrieve 
+for quick search tree is used 
+
+Trie: Advanced Tree, Multiway Tree
+eg. BT : Parent node have at max 2 childrens 
+eg. if parent node can have any nuumber of children or more than 2 children then its called multiway tree
+
+TREE: NODE, EDGE
+Parent and child node
+
+2 questions 
+how data is stored : tree like stores word/sentences aka string. 
+what operation the ds provide : 
+1) Insert
+2) delete
+3) search
+4) prefix search
+
+
+applications od TRIE 
+1) prefix searching
+2) auto Completion 
+3) suggestion
+4) search history
+
+
+TRIE v/s Hashing 
+TRIE v/s Regular Expression
+
+
+*/
+
+int main(){
+    
+    return 0;
+}
---
+++ DS/trie.js
@@ -0,0 +1,29 @@
+
+/* 
+object creation 
+1) {}
+2) constructor function 
+3) new
+4) Object()
+*/
+
+/*
+this binding 
+
+function : execution Context 
+Global execution cotext
+
+ec has 3 major things
+
+1) variable environment
+2) scope chain 
+3) this keyword
+
+This binding
+1) Normal Function(Function Expression, fucntion declaraion) : undefined 
+2)  object's method : object
+3) Arrow Function: dynamic This ( mostly take this of it parents EC)
+
+
+
+*/
